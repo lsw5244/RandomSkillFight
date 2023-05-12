7f94633dcdcd9ab620556c0bf7647d2a74d6e118
이성우 <lsw5244@naver.com>
2023-05-12 17:23:29 +0900

    feat : SkillUIManager 추가
    스킬 쿨타임 표시 등을 처리하기 위한 SkillUIManager 스크립트 추가

---
+++ Assets/Scripts/SkillUIManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillUIManager : MonoBehaviour
+{
+    public Image qSkillCoolTimeImage;
+    public Image wSkillCoolTimeImage;
+    public Image eSkillCoolTimeImage;
+
+    public void StartQSkillCollTime(float coolTime)
+    {
+
+    }
+
+    IEnumerator StartQSkillCollTimeCoroutine(float coolTime)
+    {
+        yield return null;
+    }
+
+    public void StartWSkillCollTime(float coolTime)
+    {
+
+    }
+
+    IEnumerator StartWSkillCollTimeCoroutine(float coolTime)
+    {
+        yield return null;
+    }
+
+    public void StartESkillCollTime(float coolTime)
+    {
+
+    }
+
+    IEnumerator StartESkillCollTimeCoroutine(float coolTime)
+    {
+        yield return null;
+    }
+}
